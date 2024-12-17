f1eb7a19d352f54288ce0fe70686dfdcd04cc489
mskk3215 <m.kaaiweb@gmail.com>
2024-12-18 00:12:40 +0900

    feat:Add message table and model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,7 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_241_216_145_550) do
+ActiveRecord::Schema.define(version: 20_241_217_150_705) do
+  create_table 'messages', force: :cascade do |t|
+    t.integer 'user_id'
+    t.string 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_messages_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'provider', default: 'email', null: false
     t.string 'uid', default: '', null: false
