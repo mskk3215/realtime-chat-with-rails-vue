4e088df27d07ab713da986d9cc9e542870d33f02
mskk3215 <m.kaaiweb@gmail.com>
2024-12-25 23:58:54 +0900

    feat: Add Welcome.vue

---
+++ frontend/src/views/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <!-- <h1>{{ 3 + 3 }}</h1> -->
+    <!-- <p>{{ subtitle }}</p> -->
+    <p v-if="isEnabled">{{ subtitle }}</p>
+    <button @dblclick="toggle">トグルする</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '初めてのVue.jsアプリです!',
+      subtitle: 'ようこそ',
+      isEnabled: true,
+    };
+  },
+  computed: {
+    text() {
+      if (this.isEnabled) {
+        return 'こんにちは！';
+      }
+      return 'さよなら!';
+    },
+  },
+  methods: {
+    toggle() {
+      this.isEnabled = !this.isEnabled;
+    },
+  },
+};
+</script>
