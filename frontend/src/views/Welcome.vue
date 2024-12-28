43ab57b95199da4a527ba9d8b952324c33d1b428
mskk3215 <m.kaaiweb@gmail.com>
2024-12-28 23:51:16 +0900

    feat: Add "TestComponent"

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -3,14 +3,24 @@
     <h1>{{ title }}</h1>
     <!-- <h1>{{ 3 + 3 }}</h1> -->
     <!-- <p>{{ subtitle }}</p> -->
-    <p v-if="isEnabled">{{ subtitle }}</p>
+    <!-- <p v-if="isEnabled">{{ subtitle }}</p>
     <button @dblclick="toggle">トグルする</button>
-    <p>{{ text }}</p>
+    <p>{{ text }}</p> -->
+    <TestComponent
+      @toggle="toggle"
+      message="コンポーネントにデータが渡されています"
+    />
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '@/components/TestComponent.vue';
+
 export default {
+  components: {
+    TestComponent,
+  },
   data() {
     return {
       title: '初めてのVue.jsアプリです!',
