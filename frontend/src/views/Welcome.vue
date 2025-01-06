af2d4d6b0ca33df1ef971172571181164e93ca02
mskk3215 <m.kaaiweb@gmail.com>
2025-01-06 23:46:29 +0900

    chore: Remove TestComponent

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -1,52 +1,14 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <!-- <h1>{{ 3 + 3 }}</h1> -->
-    <!-- <p>{{ subtitle }}</p> -->
-    <!-- <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">トグルする</button>
-    <p>{{ text }}</p> -->
-    <TestComponent
-      @toggle="toggle"
-      message="コンポーネントにデータが渡されています"
-    />
-    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
-import TestComponent from '@/components/TestComponent.vue';
 
 export default {
   components: {
-    TestComponent,
-  },
-  data() {
-    return {
-      title: '初めてのVue.jsアプリです!',
-      subtitle: 'ようこそ',
-      isEnabled: true,
-    };
-  },
-  computed: {
-    text() {
-      if (this.isEnabled) {
-        return 'こんにちは！';
-      }
-      return 'さよなら!';
-    },
-  },
-  methods: {
-    toggle() {
-      this.isEnabled = !this.isEnabled;
-    },
   },
 };
 </script>
 
-<style scoped>
-p {
-  font-size: 20px;
-  color: red;
 }
 </style>
